2bc9ffbc35edfd1682dcb64c7cd16844173f3da2
dnxbjyj <dnxbjyj@126.com>
2018-09-21 00:38:58 +0800

    daily commit

--- py-code-templage.py
+++ py-code-templage.py
@@ -1,16 +1,18 @@
 # coding:utf-8
 #
-from __future__ import unicode_literal
+from __future__ import unicode_literals
 import traceback
 import sys
+import requests
 reload(sys)
 sys.setdefaultencoding('utf-8')
 
 def sample():
-    a = 1/0
+    pass
 
 if __name__ == '__main__':
     try:
         sample()
+        print 'end'
     except Exception as e:
-        print 'error occurs! traceback: {0}'.format()
+        print 'error occurs! detail:\n{0}'.format(traceback.format_exc())
